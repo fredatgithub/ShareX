fea1938e243bcd682ce75b7f47f6857fa71fcc6f
Jaex <Jaex@users.noreply.github.com>
2026-05-19 10:03:16 +0300

    Refactor canvas recentring logic to use a counter for passes instead of a boolean flag

--- ShareX.ImageEditor/Presentation/Views/EditorView.axaml.cs
+++ ShareX.ImageEditor/Presentation/Views/EditorView.axaml.cs
@@ -72,7 +72,7 @@ namespace ShareX.ImageEditor.Presentation.Views
         private int _pendingAutoCopyImageVersion;
         private bool _overlayCanvasLayoutUpdatePending;
         private bool _isAdjustingCanvasScrollOffset;
-        private bool _pendingCanvasRecentering;
+        private int _pendingCanvasRecenteringPasses;
         private Rect? _lastOverlayCanvasRect;
         private double _lastOverlayCanvasZoom = -1;
         private double _lastRenderScaling = 1.0;
@@ -324,7 +324,7 @@ namespace ShareX.ImageEditor.Presentation.Views
 
         internal void QueueCanvasRecentering()
         {
-            _pendingCanvasRecentering = true;
+            _pendingCanvasRecenteringPasses = 4;
         }
 
         private void UpdateCanvasPanHostLayout()
@@ -368,14 +368,16 @@ namespace ShareX.ImageEditor.Presentation.Views
                 ? Avalonia.Controls.Primitives.ScrollBarVisibility.Auto
                 : Avalonia.Controls.Primitives.ScrollBarVisibility.Hidden;
 
+            bool hasPendingCanvasRecentering = _pendingCanvasRecenteringPasses > 0;
+
             if (!widthChanged && !heightChanged)
             {
-                if (_pendingCanvasRecentering)
+                if (hasPendingCanvasRecentering)
                 {
                     Dispatcher.UIThread.Post(() =>
                     {
                         SetCanvasScrollOffset(scrollViewer, GetDefaultCanvasScrollOffset(scrollViewer));
-                        _pendingCanvasRecentering = false;
+                        _pendingCanvasRecenteringPasses--;
                     }, DispatcherPriority.Render);
                 }
 
@@ -384,7 +386,7 @@ namespace ShareX.ImageEditor.Presentation.Views
 
             Dispatcher.UIThread.Post(() =>
             {
-                Vector targetOffset = _pendingCanvasRecentering
+                Vector targetOffset = hasPendingCanvasRecentering
                     ? GetDefaultCanvasScrollOffset(scrollViewer)
                     : ClampCanvasScrollOffset(scrollViewer, GetDefaultCanvasScrollOffset(scrollViewer) + offsetDelta);
 
@@ -393,7 +395,10 @@ namespace ShareX.ImageEditor.Presentation.Views
                     SetCanvasScrollOffset(scrollViewer, targetOffset);
                 }
 
-                _pendingCanvasRecentering = false;
+                if (hasPendingCanvasRecentering)
+                {
+                    _pendingCanvasRecenteringPasses--;
+                }
             }, DispatcherPriority.Render);
         }
 
@@ -678,6 +683,7 @@ namespace ShareX.ImageEditor.Presentation.Views
                 if (vm.PreviewImage != null)
                 {
                     bool isInitialImageLoad = _editorCore.SourceImage == null;
+                    _zoomController.ResetScrollViewerOffset();
                     LoadImageFromViewModel(vm);
                     if (isInitialImageLoad)
                     {
